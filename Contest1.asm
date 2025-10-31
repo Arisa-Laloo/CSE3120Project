6744c94a8f8628847f535d0e802cc1ad2e5e539d
Arisa-Laloo <alaloo2022@my.fit.edu>
2025-10-31 18:24:24 -0400

    base level implementation

--- Contest1.asm
+++ Contest1.asm
@@ -1,7 +1,7 @@
 INCLUDE Irvine32.inc
 
 .data 
-promp BYTE "Guess a number between 1 and 50", 0
+prompt BYTE "Guess a number between 1 and 50", 0
 promptHigh BYTE "Too High!", 0
 promptLow BYTE "Too Low!", 0
 promptCorrect BYTE "Correct", 0
@@ -22,6 +22,44 @@ inc eax			 ; shift range to 1 - 50
 mov target, eax
 call WriteDec	 ; print decimal value in eax 
 call Crlf
+
+GameLoop:
+	inc attempts
+	mov edx, OFFSET prompt
+	call WriteString
+	call Crlf
+	call ReadDec
+	mov guess, eax
+
+	cmp eax, target
+	jb TooLow
+	je Correct
+	ja TooHigh 
+	
+
+
+TooHigh:
+	mov edx, OFFSET promptHigh
+	call WriteString
+	call Crlf
+	jmp GameLoop
+
+TooLow:
+	mov edx, OFFSET promptLow
+	call WriteString 
+	call Crlf
+	jmp GameLoop
+
+
+Correct:
+	mov edx, OFFSET promptCorrect
+	call WriteString
+	call Crlf
+	mov eax, attempts
+	call WriteDec
+	call Crlf
+
+
 
 exit
 main ENDP
